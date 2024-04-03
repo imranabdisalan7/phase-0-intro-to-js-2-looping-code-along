617d62ed7a3f474b7fe209008e76ba7f20cfe72b
imranabdisalan7 <agmraan@gmail.com>
2024-04-03 10:28:01 +0300

    add newLooping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return messages
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number --
+    }
+}
+
+countDown(10);
